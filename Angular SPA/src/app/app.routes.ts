69b2ebb78c0d734767ff5443b8de848c94a11f4a
Nitesh N D <niteshdwaraka@gmail.com>
2026-02-23 23:20:56 +0530

    Delete Angular SPA directory

--- Angular SPA/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home';
-import { StudentComponent } from './student/student';
-
-export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'student', component: StudentComponent }
-];
