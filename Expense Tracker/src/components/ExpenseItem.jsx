72bec6017c11db374f43d1e5eb56cf0ecc5cb3fb
Nitesh N D <niteshdwaraka@gmail.com>
2026-02-23 21:55:29 +0530

    Update ExpenseItem.jsx

--- Expense Tracker/src/components/ExpenseItem.jsx
+++ Expense Tracker/src/components/ExpenseItem.jsx
@@ -1,35 +1,38 @@
 function ExpenseItem({ expense, setExpenses, setEditingExpense }) {
   return (
-   <li className="bg-white p-4 rounded-lg shadow flex justify-between items-center hover:shadow-md transition">
-  <div>
-    <h3 className="text-lg font-semibold text-gray-800">
-      {expense.title}
-    </h3>
-    <p className="text-sm text-gray-500">
-      ₹{expense.amount} • {expense.date}
-    </p>
-  </div>
+  <li className="bg-white border border-gray-300 px-4 py-3 flex justify-between items-center hover:bg-gray-50 transition">
+    
 
-  <div className="flex gap-3">
-    <button
-      onClick={() => setEditingExpense(expense)}
-      className="text-blue-600 font-medium hover:underline"
-    >
-      Edit
-    </button>
-    <button
-      onClick={() =>
-        setExpenses((prev) =>
-          prev.filter((item) => item.id !== expense.id)
-        )
-      }
-      className="text-red-600 font-medium hover:underline"
-    >
-      Delete
-    </button>
-  </div>
-</li>
-  );
+    <div>
+      <h3 className="text-sm font-semibold text-gray-900">
+        {expense.title}
+      </h3>
+      <p className="text-xs text-gray-500">
+        ₹{expense.amount} • {expense.date}
+      </p>
+    </div>
+
+    <div className="flex gap-4 text-sm">
+      <button
+        onClick={() => setEditingExpense(expense)}
+        className="text-red-600 hover:underline"
+      >
+        Edit
+      </button>
+
+      <button
+        onClick={() =>
+          setExpenses((prev) =>
+            prev.filter((item) => item.id !== expense.id)
+          )
+        }
+        className="text-gray-500 hover:text-red-600 hover:underline"
+      >
+        Delete
+      </button>
+    </div>
+  </li>
+);
 }
 
 export default ExpenseItem;
